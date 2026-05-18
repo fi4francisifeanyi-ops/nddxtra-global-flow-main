fb304c42115cf8faf0f94fd9dcffcc3e1935c355
fi4francisifeanyi-ops <fi4francisifeanyi@gmail.com>
2026-05-18 18:51:58 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -4,7 +4,5 @@ app = Flask(_name_)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Website works!</h1>
-    <p>Your vercel deployment is successful.</p>
-    """
+  return"<h1>Website works!</h1>"
+    
