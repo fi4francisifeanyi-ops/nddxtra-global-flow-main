d154dfe3717b8242175e8fd1f3f766afae5d19c6
fi4francisifeanyi-ops <fi4francisifeanyi@gmail.com>
2026-05-18 21:10:30 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,37 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def home():
-  return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>My Website</title>
-        <style>
-            body {
-                font-family: Arial;
-                background: #0f172a;
-                color: white;
-                text-align: center;
-                padding-top: 100px;
-            }
-            .card {
-                background: #1e293b;
-                padding: 40px;
-                border-radius: 12px;
-                display: inline-block;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="card">
-            <h1>🚀 My Website is Live</h1>
-            <p>Deployed successfully on Vercel</p>
-        </div>
-    </body>
-    </html>
-    """
-    """
+    return "Hello from Flask on Vercel!"
     
